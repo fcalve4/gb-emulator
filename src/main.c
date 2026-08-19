56c4e34a2aeb4fb162deff41de5494eb5bb184c5
fcalve <freddycalve@gmail.com>
2026-08-19 08:57:57 -0400

    created baisc SDL_PollEvent loop and allowed quitting window

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <SDL2/SDL.h>
 
 #include "rom.h"
@@ -15,11 +16,42 @@ int main(int argc, char *argv[]) {
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
     
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    uint8_t* rom_buffer = load_rom("roms/Tetris.gb");
     
-    SDL_Delay(6000);
+    // Grab the ROM and load it into memory map
+    long rom_size = 0;
+    uint8_t* rom_buffer = load_rom("roms/Tetris.gb", &rom_size);
+    load_into_memory(rom_buffer, rom_size);
+    
+    int running = 1;
+    SDL_Event event;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    running = 0;
+                    break;
 
+                case SDL_KEYDOWN:
+                    if (event.key.keysym.sym == SDLK_ESCAPE) {
+                        running = 0;
+                    }
+                    break;
+            }
+        }
+
+        // update / game logic here
+
+        SDL_SetRenderDrawColor(renderer, 20, 20, 20, 255);
+        SDL_RenderClear(renderer);
+
+        // rendering here
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
